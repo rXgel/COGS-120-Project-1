45e4eb220e50df29245cb374dc57eb8bd1092e6f
Leslie Nguyen <len007@ucsd.edu>
2021-03-14 18:21:27 -0700

    updated all files to correct functionality

--- routes/projects.js
+++ routes/projects.js
@@ -4,7 +4,12 @@
  */
 
 var data = require("../data.json");
+var subtasks = require("../subtasks.json");
 
 exports.view = function(req, res){
   res.render('projects', data);
+};
+
+exports.view = function(req, res){
+  res.render('projects', subtasks);
 };
